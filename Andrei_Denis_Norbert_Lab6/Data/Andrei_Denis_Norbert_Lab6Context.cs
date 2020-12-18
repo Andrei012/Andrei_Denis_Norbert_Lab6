b66102b2c094c9a97e2d787360918576dcced884
DENIS NORBERT ANDREI <andrei.denis164133@student.ubbcluj.ro>
2020-12-18 16:48:40 +0200

    Add project files.

---
+++ Andrei_Denis_Norbert_Lab6/Data/Andrei_Denis_Norbert_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Andrei_Denis_Norbert_Lab6.Models;
+
+namespace Andrei_Denis_Norbert_Lab6.Data
+{
+    public class Andrei_Denis_Norbert_Lab6Context : DbContext
+    {
+        public Andrei_Denis_Norbert_Lab6Context (DbContextOptions<Andrei_Denis_Norbert_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Andrei_Denis_Norbert_Lab6.Models.Book> Book { get; set; }
+    }
+}
---
+++ Andrei_Denis_Norbert_Lab6/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Andrei_Denis_Norbert_Lab6.Data;
+using Andrei_Denis_Norbert_Lab6.Models;
+
+namespace Andrei_Denis_Norbert_Lab6.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Andrei_Denis_Norbert_Lab6.Data.Andrei_Denis_Norbert_Lab6Context _context;
+
+        public IndexModel(Andrei_Denis_Norbert_Lab6.Data.Andrei_Denis_Norbert_Lab6Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
